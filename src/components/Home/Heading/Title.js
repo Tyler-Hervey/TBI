0ea96f6998f72bb3c187bd4ef4cf8470f9b91b28
cory <parravano.cory@gmail.com>
2020-08-05 06:16:53 -0700

    add: title style

---
+++ src/components/Home/Heading/Title.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./heading.module.scss"
+
+const Title = props => {
+  return <h2 className={styles.title}>{props.children}</h2>
+}
+
+export default Title
