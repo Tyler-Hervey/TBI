47fe28ca5dbc6d82de6cd959235ea38ec08c78a7
Tydoo <tyler@moximetrics.com>
2020-10-02 11:36:23 -0500

    fixed casing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,18 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import "./index.styles.css"
-import Hero from "../components/home/hero/hero"
-import OurClients from "../components/home/OurClients/OurClients"
-import WhyInfluencers from "../components/home/whyInfluencers/WhyInfluencers"
-import Results from "../components/home/results/Results"
-import CampaignPhases from "../components/home/campaignPhases/compaignPhases"
-import VideoModule from "../components/home/VideoModule/VideoModule"
-import Cta from "../components/home/cta/Cta"
+import Hero from "../components/Home/Hero/hero"
+import OurClients from "../components/Home/OurClients/OurClients"
+import WhyInfluencers from "../components/Home/whyInfluencers/WhyInfluencers"
+import Results from "../components/Home/results/Results"
+import CampaignPhases from "../components/Home/campaignPhases/compaignPhases"
+import VideoModule from "../components/Home/VideoModule/VideoModule"
+import Cta from "../components/Home/CTA/Cta"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Hero></Hero>
+    <Hero />
     <OurClients />
     <WhyInfluencers />
     <Results />
