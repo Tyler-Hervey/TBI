14a4eb1a25ee428f8ccfd11e7c291986f13aa48b
Tydoo <tyler@moximetrics.com>
2020-10-05 11:36:10 -0500

    removing duplicate git files... problems due to case insensitive local

--- src/components/Home/Header/Navbar/mobileMenu/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from "./backdrop.module.scss"
-
-const Backdrop = props => {
-  return <div onClick={props.click} className={styles.backdrop}></div>
-}
-
-export default Backdrop
--- src/components/Home/Heading/Title.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from "./home-heading.module.scss"
-
-const Title = props => {
-  return <h2 className={styles.title}>{props.children}</h2>
-}
-
-export default Title
